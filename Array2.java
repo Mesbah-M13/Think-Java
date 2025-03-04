2c4eed476d5d7983bf06c09717e151cd68e4fa02
Mesbah-Himel <72723412+Mesbah-M13@users.noreply.github.com>
2025-03-04 14:04:33 +0600

    Update Array2.java
    A program for reading and printing values of an Array

--- Array2.java
+++ Array2.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+// READ and PRINT values of an ARRAY
+
 public class Array2 {
     public static void main(String[] args) {
         // input for array size
@@ -12,7 +14,7 @@ public class Array2 {
         // input for array elements
         int[] newArr = new int[size];
 
-        System.out.println("Your entered input:");
+        System. out.println("Your entered input:");
         for (int i = 0; i < size; i++) {
             newArr[i] = in.nextInt();
         }
