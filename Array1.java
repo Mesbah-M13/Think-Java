4fe1f8cc215beedcd3fa9b90fc4c61c98354904e
Mesbah-Himel <72723412+Mesbah-M13@users.noreply.github.com>
2025-03-04 14:02:50 +0600

    Update Array1.java

--- Array1.java
+++ Array1.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+// Print Values of an Array
+
 public class Array1 {
 
     public static void main(String[] args) {
@@ -11,16 +13,20 @@ public class Array1 {
             myArr[0] = 22;
             myArr[1] = 2;
             myArr[2] = 20;
-
+// PATH -1
         // for (int i = 0; i < myArr.length; i++) {
         //     myArr[i] = in.nextInt();
         // }
         // for (int i = 0; i < myArr.length; i++) {
         //     System.out.print(myArr[i] + " ");
         // }
-
+// PATH -2
+//  for (int i: myArr) {
+//            System.out.print(Arrays.toString(myArr));
+//        }
+// PATH -3
         for (int i = 0; i < myArr.length; i++) {
-        myArr[i]= in.nextInt();       
+        myArr[i]= in.nextInt();       // BEST ⭐
      }
 
      System.out.print(Arrays.toString(myArr));
